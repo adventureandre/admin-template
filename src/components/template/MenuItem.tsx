58df49fdfebaa32adae6a8dd020549059249c75b
Andre Luiz <adventureandre@hotmail.com>
2023-02-09 19:11:59 -0300

    Criaçao do modo dark!!

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,21 +1,49 @@
 import Link from "next/link";
 
 interface MenuItemProps {
-    url: string,
+    url?: string,
     texto: string,
-    icone: any
+    icone: any,
+    className?: string,
+    onClick?: (event: any) => void
 }
 
 export default function MenuItem(props: MenuItemProps) {
-    return (
-        <li className="hover:bg-gray-500">
-            <Link className="flex flex-col justify-center items-center h-20 w-20" href={props.url}>
+    function renderizarLink() {
+        let link = (
+            <Link className={`
+                flex flex-col justify-center items-center 
+                 h-20 w-20
+                 dark:text-gray-200
+                 ${props.className}
+                 `}
+                  href={(props.url ? props.url : '')}>
                 {props.icone}
-                <span className="text-xs font-light text-gray-600">
+                <span className="text-xs font-light">
                     {props.texto}
                 </span>
             </Link>
+        )
+        let icon = (
+            <div className={`
+                flex flex-col justify-center items-center 
+                 h-20 w-20
+                 dark:text-gray-200
+                 ${props.className}
+                 `}>
+                {props.icone}
+                <span className="text-xs font-light">
+                    {props.texto}
+                </span>
+            </div>
+        )
 
+        return props.url ? link : icon
+    }
+
+    return (
+        <li onClick={props.onClick} className="hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer">
+            {renderizarLink()}
         </li>
     )
 }
--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,14 +1,35 @@
 import MenuItem from "@/components/template/MenuItem";
-import {IconeAjuste, IconeCasa, IconeSino} from "@/components/icons";
+import {IconeAjuste, IconeCasa, IconeSair, IconeSino} from "@/components/icons";
+import Logo from "@/components/Logo";
 
 export default function MenuLateral() {
     return (
-        <aside>
-          <ul>
+        <aside className="flex flex-col
+         bg-gray-200
+        text-gray-700
+        dark:bg-gray-900
+        dark:text-gray-200"
+        >
+            <div className="
+            flex flex-col items-center justify-center
+            bg-gradient-to-r from-indigo-500 via-indigo-500 to-purple-800
+            h-20 w-20
+            ">
+                <Logo/>
+            </div>
+          <ul className="flex-grow">
               <MenuItem url='/' texto="Início" icone={IconeCasa}/>
               <MenuItem url='/ajustes' texto="Ajustes" icone={IconeAjuste}/>
               <MenuItem url='/notificacoes' texto="Notificações" icone={IconeSino}/>
           </ul>
+            <ul>
+                <MenuItem texto="Sair" icone={IconeSair}
+                onClick={() => console.log('aki')}
+                className="text-red-600 dark:text-red-400
+                 hover:bg-red-400 hover:text-white
+                 dark:hover:text-white
+                 "/>
+            </ul>
         </aside>
     )
 }
