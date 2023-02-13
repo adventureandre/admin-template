5061755027f31ec854d25bb232a1fba14530ada9
Andre Luiz <adventureandre@hotmail.com>
2023-02-13 11:45:19 -0300

    Criando AppContext.tsx criando um Hook

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,11 +1,17 @@
 import Layout from "@/components/template/Layout";
+import {AppConsumer} from "@/data/context/AppContext";
+import useAppData from "@/data/hook/useAppData";
 
 export default function Notificacoes() {
+   const dados = useAppData()
     return (
         <>
             <Layout titulo="Notificações"
                     subtitulo="Gerencie as suas Notificaçoes">
-                <h3>Conteudo!!</h3>
+                <AppConsumer>
+                    {dados => <h3>{dados.nome}</h3>}
+                </AppConsumer>
+                {dados.nome}
             </Layout>
         </>
     )
